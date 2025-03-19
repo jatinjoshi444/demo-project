3096f781ff8ceab9540fa7969476ec648b610dc9
Jatin Joshi <jatinjoshi905@gmail.com>
2025-03-19 11:13:08 +0530

    Some code updated on js file

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
         'url(/assets/download (1).jpg)',
         'url(/assets/download.jpg)'
     ];
-
-    let currentIndex = 0;
     let currentImageIndex = 0;
     const header = document.getElementById('header');
 
@@ -17,31 +15,35 @@ document.addEventListener('DOMContentLoaded', function () {
         header.style.backgroundImage = image[currentImageIndex];
     }
 
-    setInterval(changeBackgroundImage, 2000);
-
-    // slider
-    function changeImage(direction) {
-        currentIndex += direction;
-
-        if (currentIndex < 0) {
-            currentIndex = image.length - 1;
-        } else if (currentIndex >= image.length) {
-            currentIndex = 0;
-        }
-
-        document.getElementById('migration-carausal').style.backgroundImage = image[currentIndex];
-    }
-
-    document.getElementById('migration-carausal').style.backgroundImage = image[currentIndex];
-
+    setInterval(changeBackgroundImage, 3000);
     // navbar
     menuToggle.addEventListener('click', function () {
         navbar.classList.toggle('show');
     });
-
-    // open para
-    function toggleAccordion(element) {
-        element.parentElement.classList.toggle("active");
-    }
 });
+  // slider
+  const images = [
+    'url(/assets/bg-image.jpg)',
+    'url(/assets/background.jpg)',
+    'url(/assets/download (1).jpg)',
+    'url(/assets/download.jpg)'
+];
+  let currentIndex = 0;
+  function changeImage(direction) {
+    currentIndex += direction;
 
+    if (currentIndex < 0) {
+        currentIndex = image.length - 1;
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    document.getElementById('migration-carausal').style.backgroundImage = images[currentIndex];
+}
+
+document.getElementById('migration-carausal').style.backgroundImage = images[currentIndex];
+
+// open para
+function toggleAccordion(element) {
+    element.parentElement.classList.toggle("active");
+}
